6a3930c93c03ae65f29d178d5e21ce6969de1e4b
username <email@email.com>
2025-04-22 12:07:15 +0700

    praktikum 01

---
+++ praktikum01/variable_user.php
@@ -0,0 +1,12 @@
+<?php
+//mendefinisikan variable
+
+$nama = 'Anggia';
+$umur = 18;
+$berat = 40.40;
+
+echo 'Nama saya ' . $nama;
+echo '<br/>Umur : ' . $umur . 'Tahun';
+echo '<br/>Berat: ' . $berat . 'Kg';
+
+?>
