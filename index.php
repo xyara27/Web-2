608da11e0cf3efa3bc9a7d368657723a3a638ef4
username <email@email.com>
2025-06-27 09:13:18 +0700

    praktikum 03

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Freelancer - Start Bootstrap Theme</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    < id="page-top">
+        <!-- Navigation-->
+        <?php include_once 'partials/navbar.php';?>
+        <!-- Masthead-->
+        <?php include_once 'partials/hero.php';?>
+        <!-- Portfolio Section-->
+        <?php include_once 'partials/sections/portfolio.php';?>
+        <!-- About Section-->
+        <?php include_once 'partials/sections/about.php';?>
+        <!-- Contact Section-->
+        <?php include_once 'partials/sections/contact.php';?>
+        <!-- Footer-->
+        <?php include_once 'partials/footer.php';?>
+        <!-- Copyright Section-->
+        <div class="copyright py-4 text-center text-white">
+            <div class="container"><small>Copyright &copy; Your Website 2023</small></div>
+        </div>
+        <!-- Portfolio Modals-->
+        <!-- Portfolio Modal 1-->
+        <?php include_once 'partials/modals/portfolio-1.php';?>
+        <!-- Portfolio Modal 2-->
+        <?php include_once 'partials/modals/portfolio-2.php';?>
+        <!-- Portfolio Modal 3-->
+        <?php include_once 'partials/modals/portfolio-3.php';?>
+        <!-- Portfolio Modal 4-->
+        <?php include_once 'partials/modals/portfolio-4.php';?>
+        <!-- Portfolio Modal 5-->
+        <?php include_once 'partials/modals/portfolio-5.php';?>
+        <!-- Portfolio Modal 6-->
+        <?php include_once 'partials/modals/portfolio-6.php';?>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <!-- * *                               SB Forms JS                               * *-->
+        <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+    </body>
+</html>
---
+++ task/about.php
@@ -0,0 +1,18 @@
+<section class="bg-light py-5">
+  <div class="container px-5">
+    <div class="row gx-5 justify-content-center">
+      <div class="col-xxl-8">
+        <div class="text-center my-5">
+          <h2 class="display-5 fw-bolder"><span class="text-gradient d-inline">About Aura's</span></h2>
+          <p class="lead fw-light mb-4">My name is Aura and I help brands grow.</p>
+          <p class="text-muted">Welcome to my corner of the internet! I specialize in turning visions into reality and helping brands reach their full potential. From marketing strategies to creative direction, I'm here to guide you through the journey of brand growth. Let's collaborate and make something amazing happen!</p>
+          <div class="d-flex justify-content-center fs-2 gap-4">
+            <a class="text-gradient" href="https://twitter.com/aura"><i class="bi bi-twitter"></i></a>
+            <a class="text-gradient" href="https://linkedin.com/in/aura"><i class="bi bi-linkedin"></i></a>
+            <a class="text-gradient" href="https://github.com/xyara27"><i class="bi bi-github"></i></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
